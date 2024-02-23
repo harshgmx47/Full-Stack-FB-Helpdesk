384a3ab261d03e3d797bd65de12404b6d283d0ec
harsh vadera <harshgmx47@gmail.com>
2024-02-23 14:23:48 +0530

    webhook

--- Backend/routes/webhooks.js
+++ Backend/routes/webhooks.js
@@ -2,6 +2,28 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 
+app.get("/messaging-webhook", (req, res) => {
+  const VERIFY_TOKEN = process.env.WEBHOOK_VERIFY_TOKEN;
+
+  // Parse the query params
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+  
+    // Check if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Check the mode and token sent is correct
+      if (mode === "subscribe" && token === process.env.WEBHOOK_VERIFY_TOKEN) {
+        // Respond with the challenge token from the request
+        console.log("WEBHOOK_VERIFIED");
+        res.status(200).send(challenge);
+      } else {
+        // Respond with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+      }
+    }
+  });
+
 router.get("/", async (req, res) => {
   //   Your verify token. Should be a random string.
   let VERIFY_TOKEN = process.env.WEBHOOK_VERIFY_TOKEN;
