b969120b066699e379dd5150e44b40db0d75baf8
harsh vadera <harshgmx47@gmail.com>
2024-02-26 20:53:16 +0530

    changes

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -4,9 +4,8 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-    <BrowserRouter basename={App}>
-      {/* Wrap your App component with BrowserRouter and set basename */}
+   
       <App />
-    </BrowserRouter>
+  
     </React.StrictMode>
 );
