4c870a05e39407de4b318ce16f96e13f4d3428bd
harsh vadera <harshgmx47@gmail.com>
2024-02-29 16:48:48 +0530

    url changed

--- FrontEnd/src/config.js
+++ FrontEnd/src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5050/";
+// export const BASE_URL = "http://localhost:5050/";
 export const APP_ID = 1084249936107793;
 //715200024161591;
-// export const BASE_URL = "https://full-stack-fb-helpdesk.onrender.com/";
+export const BASE_URL = "https://webservice-helpdesk.onrender.com/";
